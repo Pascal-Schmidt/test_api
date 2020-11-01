3a83e882cbdd88e40594f6af8cce97b62dcc1179
Pascal Schmidt <pascals@sfu.ca>
2020-11-01 10:55:00 -0800

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -17,19 +17,3 @@ library(plumber)
 function(msg = "") {
     list(msg = paste0("The message is: '", msg, "'"))
 }
-
-#* Plot a histogram
-#* @png
-#* @get /plot
-function() {
-    rand <- rnorm(100)
-    hist(rand)
-}
-
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b) {
-    as.numeric(a) + as.numeric(b)
-}
